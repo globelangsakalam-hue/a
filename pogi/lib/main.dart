6fad90b8d9b1c8b3c74a89e33a6a1ddaa5e0c2b7
70x7 <globelangsakalam@gmail.com>
2026-09-11 02:16:42 +0000

    main.dart

--- pogi/lib/main.dart
+++ pogi/lib/main.dart
@@ -12,10 +12,21 @@ class MyApp extends StatelessWidget{
           debugShowCheckedModeBanner: false,
           home: Scaffold(
             body: Center(
-              child: Text('Hello World')
-
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Hilu, rworld!', 
+                  style: TextStyle(
+                    fontSize:50, 
+                    fontWeight: FontWeight.bold, 
+                    color: Colors.red,
+                    )
+                  ), 
+                  Text('Pogi daw c Sir Rodney..'),
+                ],
+              ),
             ),
-
           ),
         );
       }
